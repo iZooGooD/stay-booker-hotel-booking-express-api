23ce0ff8e20804020451b2fc8ce94364709a6236
iZooGooD <lakshmanchoudhary020@gmail.com>
2024-01-25 09:04:44 +0530

    initial commit

---
+++ server/routes/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.json({ message: 'express JSON' });
+});
+
+export default router;
