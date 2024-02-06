2252ec2cda1ee0dea0db4035bfb98ed4ef5cf449
Adam Basha <110662505+Bashamega@users.noreply.github.com>
2024-02-06 17:06:07 +0200

    Update server/utils/validators.js
    Co-authored-by: Lakshman Chaudhary <lakshmanchoudhary020@gmail.com>

--- server/utils/validators.js
+++ server/utils/validators.js
@@ -17,7 +17,7 @@ export async function UserExists(email){
     }
 };
 export function validatePhoneNumber(input_str) {
-    var re = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+    const re = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
   
     return re.test(input_str);
   }
