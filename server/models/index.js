925e16504aef9ab5ab9ee960dc9a5a98e83e4052
iZooGooD <lakshmanchoudhary020@gmail.com>
2024-02-08 09:54:17 +0530

    chore: folder restructuring

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,5 @@
 import { Sequelize } from 'sequelize';
-import config from '../config/config.js';
+import config from '../../config/config.js';
 
 const sequelize = new Sequelize(config.database, config.user, config.password, {
     host: config.host,
