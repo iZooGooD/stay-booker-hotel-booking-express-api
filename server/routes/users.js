ac179e9a87cf6e4aa85499f9286cf2a691ca7648
iZooGooD <lakshmanchoudhary020@gmail.com>
2024-02-11 09:01:49 +0530

    doc: add doc to users route

--- server/routes/users.js
+++ server/routes/users.js
@@ -8,8 +8,18 @@ import { authUser } from '../middlewares/auth.js';
 
 const router = express.Router();
 
+/**
+ * @route PUT /register
+ * @desc Registers a new user with the provided details. This route is not protected and is accessible without authentication.
+ * @access Public
+ */
 router.put('/register', registerUser);
 
+/**
+ * @route POST /login
+ * @desc Authenticates a user and returns a JWT token for accessing protected routes. This route is not protected and is accessible without authentication.
+ * @access Public
+ */
 router.post('/login', loginUser);
 
 /**
